4c2d7f950b299211bf1811d3b9dd92310f059a66
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-30 20:17:06 -0300

    feat: divider component created

---
+++ src/app/Divider.tsx
@@ -0,0 +1,9 @@
+const Divider = () => {
+  return (
+    <div className='h-3 w-[206px] bg-primary-100 rounded-lg'>
+      <div className='h-full w-[42px] bg-primary-200 rounded-lg ml-10' />
+    </div>
+  )
+}
+
+export default Divider
