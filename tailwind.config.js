d5079006c2333a0b26187156a926aa8eb26a7b49
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-30 17:53:31 -0300

    feat: added primary color and breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,27 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
+      colors: {
+        primary: {
+          100: '#FC9B9B',
+          200: '#FF6563'
+        }
+      }
     },
+    screens: {
+      xs: '768px',
+      sm: '1024px',
+      md: '1440px'
+    }
   },
-  plugins: [],
+  plugins: []
 }
