927aa5d26e13711b13976cc9ffa327de1f1f2afa
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-30 20:18:57 -0300

    feat: created render skills

--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+import skills from '@/constants/skills'
 import Avatar from '~/svg/avatar.svg'
 import Dots from '~/svg/dots.svg'
 import GithubIcon from '~/svg/github-icon.svg'
 import KeyLeft from '~/svg/key-left.svg'
 import KeyRight from '~/svg/key-right.svg'
 import LinkedinIcon from '~/svg/linkedin-icon.svg'
+import Subtitle from './Subtitle'
 
 const socialMedia = [
   {
@@ -45,10 +48,24 @@ const Profile = () => {
     </section>
   )
 
+  const renderSkills = () => (
+    <section className='flex flex-col items-center mb-[101px] mx-6'>
+      <Subtitle content='Habilidades' />
+      <div className='flex flex-row flex-wrap justify-center gap-6 w-full mt-8'>
+        {skills.map(({ name, site, iconSource }) => (
+          <a key={name} href={site} target='_blank' rel='noreferrer'>
+            <img alt={name} src={iconSource} />
+          </a>
+        ))}
+      </div>
+    </section>
+  )
+
   return (
     <aside className='card border-3 rounded-[28px] w-[604px] px-8 pb-8 leading-[29px]'>
       {renderAvatarSection()}
       {renderTitleAndSocialMedia()}
+      {renderSkills()}
     </aside>
   )
 }
