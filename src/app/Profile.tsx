2dc9ce745c78ff3973e6ca23de0193bd0909b657
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-31 06:24:21 -0300

    feat: render buttons and gif created

--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -1,12 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 import skills from '@/constants/skills'
+import Image from 'next/image'
+import CatGif from '~/gif/cat.gif'
 import Avatar from '~/svg/avatar.svg'
+import ClipIcon from '~/svg/clip-icon.svg'
 import Dots from '~/svg/dots.svg'
 import GithubIcon from '~/svg/github-icon.svg'
 import KeyLeft from '~/svg/key-left.svg'
 import KeyRight from '~/svg/key-right.svg'
 import LinkedinIcon from '~/svg/linkedin-icon.svg'
-import Subtitle from './Subtitle'
+import WhatsappIcon from '~/svg/whatsapp-icon.svg'
+import Button from '../components/Button'
+import Subtitle from '../components/Subtitle'
 
 const socialMedia = [
   {
@@ -40,7 +45,7 @@ const Profile = () => {
       <h1 className='text-4xl font-medium text-primary-200 leading-[48px]'>Full Stack</h1>
       <div className='flex flex-row gap-4 mt-5'>
         {socialMedia.map(({ name, link, icon }) => (
-          <a key={name} href={link} target='_blank'>
+          <a key={name} href={link} target='_blank' className='social-media'>
             {icon}
           </a>
         ))}
@@ -49,7 +54,7 @@ const Profile = () => {
   )
 
   const renderSkills = () => (
-    <section className='flex flex-col items-center mb-[101px] mx-6'>
+    <section className='flex flex-col items-center mb-[70px] mx-6'>
       <Subtitle content='Habilidades' />
       <div className='flex flex-row flex-wrap justify-center gap-6 w-full mt-8'>
         {skills.map(({ name, site, iconSource }) => (
@@ -61,11 +66,28 @@ const Profile = () => {
     </section>
   )
 
+  const renderButtonsAndGif = () => (
+    <section className='flex flex-row gap-6 justify-between'>
+      <div className='w-full flex flex-col gap-3 justify-end'>
+        <Button endIcon={<ClipIcon height={21} />}>Me Contratar</Button>
+        <Button endIcon={<WhatsappIcon height={21} />}>Baixar CV</Button>
+      </div>
+      <Image
+        src={CatGif}
+        alt='Gif de um gato'
+        width={206}
+        height={197}
+        className='mb-[-42px]'
+      />
+    </section>
+  )
+
   return (
     <aside className='card border-3 rounded-[28px] w-[604px] px-8 pb-8 leading-[29px]'>
       {renderAvatarSection()}
       {renderTitleAndSocialMedia()}
       {renderSkills()}
+      {renderButtonsAndGif()}
     </aside>
   )
 }
