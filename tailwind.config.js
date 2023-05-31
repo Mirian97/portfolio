52dad1555cefce5e23b03590a8b98dfca8033d88
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-31 13:06:13 -0300

    feat: removed background gradient from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,11 +8,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      },
       colors: {
         primary: {
           100: '#FC9B9B',
