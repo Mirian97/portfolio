efd51f2356c21ec517d90ec854d48d82c8764ba1
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-31 19:56:38 -0300

    feat: added box shadow variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,8 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      boxShadow: {
+        100: ' 0px 4px 4px rgba(0, 0, 0, 0.25);'
       },
       colors: {
         primary: {
