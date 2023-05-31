0e2979ad2ce2e529293f1727df8304b75a3bdb2a
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-31 15:41:03 -0300

    feat: added variant prop to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,17 @@ interface ButtonProps {
   children: React.ReactNode
   type?: 'button' | 'submit' | 'reset'
   endIcon?: React.ReactNode
+  variant?: 'contained' | 'outlined'
 }
 
-const Button = ({ children, type = 'button', endIcon }: ButtonProps) => {
+const Button = ({
+  children,
+  type = 'button',
+  endIcon,
+  variant = 'contained'
+}: ButtonProps) => {
   return (
-    <button className='button' type={type}>
+    <button className={`button button-${variant}`} type={type}>
       {children}
       {endIcon}
     </button>
