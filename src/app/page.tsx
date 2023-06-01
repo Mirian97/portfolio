16a50678eb148811fac004edf161597b5e642f27
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-01 19:59:56 -0300

    feat: added title and list about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import Title from '@/components/Title'
+import { aboutList } from '@/constants/about'
+import AboutItem from './AboutItem'
+
 const Home = () => {
-  return <main></main>
+  return (
+    <div>
+      <Title content='Sobre mim' />
+      <section className='pt-6 pb-10 xs:pt-[32px] xs:pb-[24px] flex flex-col gap-2'>
+        {aboutList.map(({ name, description }) => (
+          <AboutItem key={name} description={description} />
+        ))}
+      </section>
+    </div>
+  )
 }
 
 export default Home
