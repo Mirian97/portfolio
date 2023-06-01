37c09926b423eb59213160ef3c206bb68d1c36b5
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-01 19:59:00 -0300

    feat: created constant about list

---
+++ src/constants/about.ts
@@ -0,0 +1,27 @@
+export const aboutList = [
+  {
+    name: 'Cargo',
+    description: 'Sou Desenvolvedora Full-Stack; 💻'
+  },
+  {
+    name: 'Formação 1',
+    description: 'Me formei em desenvolvimento Full-Stack na Cubos Academy; ✨'
+  },
+  {
+    name: 'Formação 2',
+    description: 'Sou graduada em Engenharia Civil pela Universidade Paulista; 🎓'
+  },
+  {
+    name: 'Experiência',
+    description: 'Atualmente trabalho como Full-Stack Júnior Freelancer; 🤸'
+  },
+  {
+    name: 'Estudos',
+    description: 'Também estou estudando Typescript, Next.JS e TailWind CSS; 📘'
+  },
+  {
+    name: 'Me pergunte',
+    description:
+      'Você pode me perguntar sobre JavaScript, React.JS, HTML, CSS, Node.JS, Express.JS, SQL, farei o possível para responder. 💬 😁'
+  }
+]
