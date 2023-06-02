875488380e9693e7cf5a21314f6f259b960e10ad
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-02 20:01:35 -0300

    feat: added constants location, available, curiosities, languages and services

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -25,3 +25,25 @@ export const aboutList = [
       'Você pode me perguntar sobre JavaScript, React.JS, HTML, CSS, Node.JS, Express.JS, SQL, farei o possível para responder. 💬 😁'
   }
 ]
+
+export const locationAndAvailability = [
+  {
+    name: 'Localização',
+    description: 'Guarulhos / SP - Brasil'
+  },
+  {
+    name: 'Freelancer',
+    description: 'Disponível'
+  }
+]
+
+export const curiosities =
+  'Sou uma grande amante de animais de estimação 💕, e tenho a alegria de compartilhar minha vida com sete gatos adoráveis 🐈🐈‍⬛😸🙀😹😻😽. Gosto de assistir Animes e Filmes 📺, assim como ler livros 📚 que me transportam para diferentes realidades 🌍. '
+
+export const languages = [
+  'Português (nativo)',
+  'Espanhol (avançado)',
+  'Inglês (intermediário)'
+]
+
+export const services = ['Frontend', 'Backend', 'Full-Stack']
