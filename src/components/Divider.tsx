c6d9c69fd9af44c492188a976a0d7a63bcc4ed14
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-02 20:03:48 -0300

    feat: added props dividerHeight

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,6 +1,7 @@
-const Divider = () => {
+const Divider = ({ dividerHeight }: { dividerHeight?: string }) => {
+  const height = dividerHeight === 'thin' ? '2' : '3'
   return (
-    <div className='h-3 w-[206px] bg-primary-100 rounded-lg'>
+    <div className={`h-${height} w-[206px] bg-primary-100 rounded-lg`}>
       <div className='h-full w-[42px] bg-primary-200 rounded-lg ml-10' />
     </div>
   )
