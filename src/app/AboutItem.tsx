0b2fe25b3b05624089874436a0a7f3a6cb509284
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-02 19:59:29 -0300

    feat: applied class text about

--- src/app/AboutItem.tsx
+++ src/app/AboutItem.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 const AboutItem = ({ description }: { description: string }) => (
-  <div className='flex flex-row items-center gap-2 min-h-[35px] xs:min-h-[42px]'>
+  <div className='flex flex-row items-center gap-2 min-h-[35px] sm:min-h-[42px]'>
     <img
       src='/svg/done-icon.svg'
       alt='Ìcone de check'
-      className='max-w-[20px] xs:max-w-[24px]'
+      className='max-w-[20px] sm:max-w-[24px]'
     />
-    <span className='text-[16px] leading-[19px] xs:text-xl xs:leading-6 text-secondary-300'>
-      {description}
-    </span>
+    <span className='text-about'>{description}</span>
   </div>
 )
 
