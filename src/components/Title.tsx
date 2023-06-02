465c0bb05abdc8a95cb73f1504c3beb42ed38acc
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-02 20:05:57 -0300

    feat: changed xs to sm breakpoint

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,7 @@ import Divider from './Divider'
 const Title = ({ content }: { content: string }) => {
   return (
     <>
-      <h1 className='text-secondary-400 font-bold text-[32px] leading-[38px] xs:text-4xl xs:leading-[43px] mb-4'>
+      <h1 className='text-secondary-400 font-bold text-[32px] leading-[38px] sm:text-4xl sm:leading-[43px] mb-4'>
         {content}
       </h1>
       <Divider />
