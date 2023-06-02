d6f8995b71c73a6019c7fe0e8bdfbed4cca560c2
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-02 20:06:28 -0300

    feat: finished about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,56 @@
+import ProjectDivider from '@/components/ProjectDivider'
+import Subtitle from '@/components/Subtitle'
 import Title from '@/components/Title'
-import { aboutList } from '@/constants/about'
+import {
+  aboutList,
+  curiosities,
+  languages,
+  locationAndAvailability,
+  services
+} from '@/constants/about'
+import ListIcon from '~/svg/list-icon.svg'
+import StarIcon from '~/svg/star-icon.svg'
 import AboutItem from './AboutItem'
+import LanguageOrServiceList from './LanguageOrServiceList'
 
-const About = () => {
-  return (
-    <div>
-      <Title content='Sobre mim' />
-      <section className='pt-6 pb-10 xs:pt-[32px] xs:pb-[24px] flex flex-col gap-2'>
-        {aboutList.map(({ name, description }) => (
-          <AboutItem key={name} description={description} />
-        ))}
-      </section>
-    </div>
-  )
-}
+const About = () => (
+  <>
+    <Title content='Sobre mim' />
+    <section className='pt-6 pb-10 sm:pt-[32px] sm:pb-[24px] flex flex-col gap-2'>
+      {aboutList.map(({ name, description }) => (
+        <AboutItem key={name} description={description} />
+      ))}
+    </section>
+    <section className='hidden sm:flex flex-row justify-between mb-8'>
+      {locationAndAvailability.map(({ name, description }) => (
+        <div key={name} className='flex flex-row gap-1'>
+          <h5 className='font-bold text-xl leading-6 text-secondary-300 break-normal'>
+            {name}:
+          </h5>
+          <h5 className='text-xl leading-6 text-success-200'>{description}</h5>
+        </div>
+      ))}
+    </section>
+    <Subtitle content='Curiosidades' dividerHeight='thin' />
+    <p className='text-about mt-4 mb-10'>{curiosities}</p>
+    <section className='flex flex-col gap-10 sm:flex-row sm:gap-2 sm:justify-between sm:items-end'>
+      <LanguageOrServiceList
+        listIcon={<ListIcon width={8} />}
+        list={languages}
+        listWidth='158px'
+      >
+        <Subtitle content='Idiomas' dividerHeight='thin' />
+      </LanguageOrServiceList>
+      <ProjectDivider />
+      <LanguageOrServiceList
+        listIcon={<StarIcon width={12} />}
+        list={services}
+        listWidth='87px'
+      >
+        <Subtitle content='Serviços' dividerHeight='thin' />
+      </LanguageOrServiceList>
+    </section>
+  </>
+)
 
 export default About
