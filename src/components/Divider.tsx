f473f39e35469592f66fc6afd883b922b34ed68e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-03 05:56:39 -0300

    feat: added animation move left to right

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,7 +1,7 @@
 const Divider = () => {
   return (
-    <div className='h-3 w-[206px] bg-primary-100 rounded-lg'>
-      <div className='h-full w-[42px] bg-primary-200 rounded-lg ml-10' />
+    <div className='h-3 w-[206px] bg-primary-100 rounded-lg relative overflow-hidden'>
+      <div className='h-full w-[42px] bg-primary-200 rounded-lg absolute animate-move-left-to-right' />
     </div>
   )
 }
