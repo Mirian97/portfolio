71769dbd7310d133ad1d2a8392fb3927523117a2
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-03 06:53:31 -0300

    feat: add props thin to interface divider and subtitle

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,8 +1,12 @@
-const Divider = ({ dividerHeight }: { dividerHeight?: string }) => {
-  const height = dividerHeight === 'thin' ? '2' : '3'
+interface DividerProps {
+  dividerHeight?: 'thin'
+}
+
+const Divider = ({ dividerHeight }: DividerProps) => {
+  const height = dividerHeight === 'thin' ? 'h-2' : 'h-3'
   return (
     <div
-      className={`h-${height} w-[206px] bg-primary-100 rounded-lg relative overflow-hidden`}
+      className={`${height} w-[206px] bg-primary-100 rounded-lg relative overflow-hidden`}
     >
       <div className='h-full w-[42px] bg-primary-200 rounded-lg absolute animate-move-left-to-right' />
     </div>
