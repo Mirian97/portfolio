bedfd31403aa22c3c400244164e10c6fa3995e3d
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-05 17:46:40 -0300

    feat: created projects constant

---
+++ src/constants/projects.ts
@@ -0,0 +1,16 @@
+import { skillIcons } from './skills'
+
+const { mui, react, javascript, html, css, postgres, node, express, netlify } = skillIcons
+
+const projects = [
+  {
+    name: 'Pigback Finance',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVtWMkcC0kW0jK5_6h7lzDAyApE7C7bXjm6yVmle5v&s',
+    skills: [react, javascript, html, css, mui, node, express, postgres, netlify],
+    description:
+      'Gerenciamento de cobranças (pagas, pendentes vencidas dos clientes) cadastrados pela empresa.'
+  }
+]
+
+export default projects
