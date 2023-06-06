0f493f6f386cfb40863cd8918341a3788e66f9f0
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-06 15:46:18 -0300

    feat: constant contact info list created

---
+++ src/constants/contact.ts
@@ -0,0 +1,14 @@
+export const contactInfoList = [
+  {
+    name: 'E-mail',
+    description: 'mirianquispe1505@gmail.com'
+  },
+  {
+    name: 'Celular',
+    description: '(11) 9 6265-9170'
+  },
+  {
+    name: 'Freelancer',
+    description: 'Disponível'
+  }
+]
