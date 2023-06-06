b3599c0a99f116b731dfd04a6ced116078fe3014
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-06 15:45:43 -0300

    feat: info item component created

---
+++ src/components/InfoItem.tsx
@@ -0,0 +1,18 @@
+interface InfoItemProps {
+  name: string
+  description: string
+  className?: string
+}
+
+const InfoItem = ({ name, description, className }: InfoItemProps) => {
+  return (
+    <div className={`flex flex-row gap-1 items-center ${className}`}>
+      <h5 className='text-xl leading-6 font-bold text-secondary-300 break-normal'>
+        {name}:
+      </h5>
+      <h5 className='text-xl leading-6 text-success-200'>{description}</h5>
+    </div>
+  )
+}
+
+export default InfoItem
