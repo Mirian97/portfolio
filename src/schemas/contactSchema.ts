8f337a51df6d4b2d88fc587c4f233f02379c705f
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-06 21:55:41 -0300

    feat: added more validations

--- src/schemas/contactSchema.ts
+++ src/schemas/contactSchema.ts
@@ -1,12 +1,12 @@
 import * as Yup from 'yup'
 import messages from './messages'
-const { fieldRequired } = messages
+const { fieldRequired, invalidEmail, invalidCharacterLength } = messages
 
 export const contactSchema = Yup.object().shape({
   name: Yup.string().required(fieldRequired).trim(),
-  subject: Yup.string().required(fieldRequired).trim(),
-  email: Yup.string().required(fieldRequired).trim(),
-  message: Yup.string().required(fieldRequired).trim()
+  subject: Yup.string().trim(),
+  email: Yup.string().email(invalidEmail).required(fieldRequired).trim(),
+  message: Yup.string().required(fieldRequired).trim().max(400, invalidCharacterLength)
 })
 
 export type contactSchemaType = Yup.InferType<typeof contactSchema>
