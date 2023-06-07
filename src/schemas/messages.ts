0361b4ad82b1785b8659311127e37945072f73d6
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-06 21:55:15 -0300

    feat: added more messages

--- src/schemas/messages.ts
+++ src/schemas/messages.ts
@@ -1,5 +1,7 @@
 const messages = {
-  fieldRequired: 'Este campo é obrigatório'
+  fieldRequired: 'Este campo é obrigatório',
+  invalidEmail: 'Digite um e-mail válido',
+  invalidCharacterLength: 'Este campo deve ter no máximo 400 caracteres'
 }
 
 export default messages
