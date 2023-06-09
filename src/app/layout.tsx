a00b246e626f67a4132c4dd35064fd5e1b7fa519
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-09 18:28:44 -0300

    fix: modified margin bottom of section layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <body className='flex flex-col pt-8 m-auto sm:flex-row sm:items-start sm:pl-5 sm:pt-16 md:pl-[119px] lg:justify-center lg:max-w-[1440px] lg:pl-0'>
         <Menu />
         <Profile />
-        <section className='card mt-8 mb-[127px] sm:mt-12 px-6 pb-8 sm:px-10 sm:h-[1100px] rounded-[28px] sm:rounded-none sm:border-l-0 lg:rounded-[28px] lg:rounded-l-none w-full overflow-y-auto overflow-x-hidden'>
+        <section className='card mt-8 mb-[117px] sm:mt-12 px-6 pb-8 sm:px-10 sm:h-[1100px] rounded-[28px] sm:rounded-none sm:border-l-0 lg:rounded-[28px] lg:rounded-l-none w-full overflow-y-auto overflow-x-hidden'>
           {children}
         </section>
       </body>
