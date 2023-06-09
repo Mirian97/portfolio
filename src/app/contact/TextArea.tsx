213ae0b03786ce758353e443d9bdffffa312ef85
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-06-09 09:37:07 -0300

    fix: added resize and inverted the display logic of typed characters

--- src/app/contact/TextArea.tsx
+++ src/app/contact/TextArea.tsx
@@ -1,5 +1,4 @@
 import { CommonInputProps } from '@/interfaces/CommonInputProps'
-import getCharactersRemaining from '@/utils/getCharactersRemaining'
 import { LegacyRef, TextareaHTMLAttributes, forwardRef, useId } from 'react'
 
 type TextAreaProps = CommonInputProps & TextareaHTMLAttributes<HTMLTextAreaElement>
@@ -18,7 +17,6 @@ const TextAreaRef = (
   ref: LegacyRef<HTMLTextAreaElement> | undefined
 ) => {
   const id = useId()
-  const charactersRemaining = getCharactersRemaining(maxLength, value)
   return (
     <div className='input-container'>
       <label htmlFor={id}>{label}</label>
@@ -28,16 +26,14 @@ const TextAreaRef = (
           id={id}
           ref={ref}
           placeholder={placeholder}
-          className='py-3 min-h-[154px] resize-none'
+          className='py-3 px-[14px] min-h-[154px]'
           {...restProps}
           maxLength={maxLength}
         />
       </div>
       {error && <span className='text-red-600'>{errorMessage}</span>}
-      {maxLength && value !== undefined && (
-        <span className='text-secondary-150'>
-          {charactersRemaining} caracteres restantes
-        </span>
+      {typeof value === 'string' && (
+        <span className='text-secondary-150'>{value.length} caracteres</span>
       )}
     </div>
   )
