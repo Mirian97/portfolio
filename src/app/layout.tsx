200e397f9545beb093ab9f4d0dccc9f06c5219ba
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-07-19 18:28:53 -0300

    feat: applied section transition in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
-/* eslint-disable @next/next/no-page-custom-font */
+import { Sofia_Sans } from 'next/font/google'
 import Menu from './Menu'
 import Profile from './Profile'
+import SectionTransition from './SectionTransition'
 import './globals.css'
+
+const sofiaSans = Sofia_Sans({
+  subsets: ['latin'],
+  fallback: ['sans-serif'],
+  weight: ['400', '500', '700'],
+  display: 'swap'
+})
 
 export const metadata = {
   title: 'Portfólio | Sobre mim',
@@ -13,18 +21,14 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <html lang='pt-br'>
       <head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@400;500;700&display=swap'
-          rel='stylesheet'
-        />
         <link rel='icon' href='./favicon.ico' />
       </head>
-      <body className='flex flex-col pt-8 m-auto sm:flex-row sm:items-start sm:pl-5 sm:pt-16 md:pl-[119px] lg:justify-center lg:max-w-[1440px] lg:pl-0'>
+      <body
+        className={`${sofiaSans.className} flex flex-col pt-8 m-auto sm:flex-row sm:items-start sm:pl-5 sm:pt-16 md:pl-[119px] lg:justify-center lg:max-w-[1440px] lg:pl-0`}
+      >
         <Menu />
         <Profile />
-        <section className='card mt-8 mb-[117px] sm:mt-12 px-6 pb-8 sm:px-10 sm:h-[1100px] rounded-[28px] sm:rounded-none sm:border-l-0 lg:rounded-[28px] lg:rounded-l-none w-full overflow-y-auto overflow-x-hidden'>
-          {children}
-        </section>
+        <SectionTransition>{children}</SectionTransition>
       </body>
     </html>
   )
