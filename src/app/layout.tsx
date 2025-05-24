57db8c05f740fd47cf6a12ad5e4f6612e16cf729
Mirian Quispe <mirianquispe1505@gmail.com>
2025-05-24 17:40:18 -0300

    feat: enhance metadata structure and improve SEO for portfolio layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Menu from '@/features/Menu'
 import Profile from '@/features/Profile'
+import { Metadata } from 'next'
 import { Sofia_Sans } from 'next/font/google'
 import { FC, PropsWithChildren } from 'react'
 import './globals.css'
@@ -11,15 +12,67 @@ const sofiaSans = Sofia_Sans({
   display: 'swap'
 })
 
-export const metadata = {
-  title: 'Portfólio | Sobre mim',
-  description: 'Portfólio pessoal de Mirian Quispe'
+export const metadata: Metadata = {
+  title: 'Portfólio de Mirian Quispe | Projetos e Sobre Mim',
+  description:
+    'Conheça o portfólio de Mirian Quispe, desenvolvedora apaixonada por criar soluções digitais inovadoras. Explore projetos, habilidades e minha trajetória profissional.',
+  keywords: [
+    'Mirian Quispe',
+    'Mirian Quispe Montevilla',
+    'portfólio desenvolvedora',
+    'projetos web',
+    'desenvolvimento front-end',
+    'desenvolvimento full-stack',
+    'programadora'
+  ],
+  openGraph: {
+    title: 'Portfólio de Mirian Quispe | Projetos e Sobre Mim',
+    description:
+      'Descubra os projetos e a trajetória de Mirian Quispe, desenvolvedora especializada em soluções digitais criativas e funcionais.',
+    url: 'https://mirian-quispe.netlify.app/',
+    siteName: 'Portfólio de Mirian Quispe',
+    images: [
+      {
+        url: '/image/avatar.png',
+        alt: 'Portfólio de Mirian Quispe',
+        width: 500
+      }
+    ],
+    locale: 'pt_BR',
+    type: 'website'
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
+  },
+  alternates: {
+    canonical: 'https://mirian-quispe.netlify.app/'
+  },
+  viewport: 'width=device-width, initial-scale=1',
+  themeColor: '#ff6563',
+  icons: {
+    icon: '/favicon.ico'
+  },
+  other: {
+    'google-site-verification': 'W7Myai3BstAyAFaa3J8jnKssJlIuMjVoWzPSI_tg-iw'
+  }
 }
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang='pt-br'>
       <head>
+        <meta
+          name='google-site-verification'
+          content='W7Myai3BstAyAFaa3J8jnKssJlIuMjVoWzPSI_tg-iw'
+        />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='icon' href='./favicon.ico' />
       </head>
