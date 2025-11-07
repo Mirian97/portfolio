4d7a199283f65959e7a63c8bf8ec65f3771c12b3
Mirian97 <mirianquispe1505@gmail.com>
2025-11-07 20:02:16 -0300

    feat: integrate Datadog RUM
    - Added DatadogInit component
    - Initialized Datadog on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Profile from '@/features/Profile'
 import { Metadata } from 'next'
 import { Sofia_Sans } from 'next/font/google'
 import { FC, PropsWithChildren } from 'react'
+import { DatadogInit } from './datadog-init'
 import './globals.css'
 
 const sofiaSans = Sofia_Sans({
@@ -73,6 +74,7 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => {
       <body
         className={`${sofiaSans.className} flex flex-col pt-8 m-auto sm:flex-row sm:items-start sm:pl-5 sm:pt-16 md:pl-[119px] lg:justify-center lg:max-w-[1440px] lg:pl-0`}
       >
+        <DatadogInit />
         <Menu />
         <Profile />
         {children}
